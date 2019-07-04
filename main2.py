9669b385afafbf346e61ffd6ce2c64d82adf5971
Açal Habi <habi_a@etna-alternance.net>
2019-07-04 11:34:33 +0200

    fix bugsé

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@
 import car
 import cv2
 import finish
+import numpy as np
 import requests
 import time
 import trajectory
@@ -43,44 +44,15 @@ while (camera.isOpened() and laps_remaining > 0):
         laps_remaining -= 1
         chronos.append(time.time() - start)
         start = time.time()
-    
-    ## Crop the image
-    crop_img = frame[60:120, 0:160]
 
-    ## Convert to grayscale
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    direction = trajectory.direction_to_take(frame)
 
-    ## Gaussian blur
-    blur = cv2.GaussianBlur(hsv, (5, 5), 0)
-
-    lower_orange = np.array([5, 40, 40])
-    upper_orange = np.array([15, 255, 255])
-    mask = cv2.inRange(blur, lower_orange, upper_orange)
-
-    ## Find the contours of the frame
-    contours, hierarchy = cv2.findContours(mask.copy(), 1, cv2.CHAIN_APPROX_NONE)
-
-    ## Find the biggest contour (if detected)
-    if len(contours) > 0:
-        c = max(contours, key=cv2.contourArea)
-        M = cv2.moments(c)
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-        cv2.line(crop_img,(cx,0),(cx,720),(255,0,0),1)
-        cv2.line(crop_img,(0,cy),(1280,cy),(255,0,0),1)
-        cv2.drawContours(crop_img, contours, -1, (0,255,0), 1)
-
-        if cx >= 120:
-           car.turnLeft()
-        if cx < 120 and cx > 50:
-            car.turn(90)
-        if cx <= 50:
-            car.turnRight()
-    else:
-        print("I don't see the line")
-
-    ## Display the resulting frame
-    cv2.imshow('frame', crop_img)
+    if direction == 0:
+        car.turn(90)
+    elif direction == 1:
+        car.turnLeft()
+    elif direction == 2:
+        car.turnRight()
 
     ## Stop program if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
